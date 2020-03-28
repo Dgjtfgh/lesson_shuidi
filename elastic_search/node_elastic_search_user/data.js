605515a8abf2a7354fc6ae6b05e242a20df1cdd1
Dgjtfgh <1137807913@qq.com>
2020-03-28 15:27:39 +0800

    elasticsearch 全文检索 kibana 数据可视化 初步学习

---
+++ elastic_search/node_elastic_search_user/data.js
@@ -0,0 +1,44 @@
+const elasticsearch = require('elasticsearch');
+const users = require('./users.json');
+const client = new elasticsearch.Client({
+    hosts: ['http://localhost:9200']
+})
+
+client.ping({
+    requestTimeout: 30000,
+}, function(error) {
+    if (error) {
+        console.log('ElasticSearch cluster is down!');
+    } else {
+        console.log('Everything is OK');
+    }
+})
+
+// 要有索引  相当于 mongodb collection
+// client.indices.create({
+//     index: "juejin"
+// }, function(error, response, status) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('created a new index', response);
+//     }
+// })
+
+var bulk = []; 
+users.forEach(user => {
+    bulk.push({index: {
+        _index:"juejin",
+        _type:"users_list",
+        }
+    })
+    bulk.push(user);
+})
+
+client.bulk({body:bulk}, function(err, response) {
+    if (err) {
+        console.log("Failed Bulk operation".red, err);
+    } else {
+        console.log(response)
+    }
+});

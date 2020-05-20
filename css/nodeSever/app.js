0186dfac8352a3036b6c92b66c4629d4761b0d3c
Dgjtfgh <1137807913@qq.com>
2020-05-20 15:27:58 +0800

    520相册

---
+++ css/nodeSever/app.js
@@ -0,0 +1,38 @@
+(async function run() {
+    const Koa = require("koa")
+    const fs = require("fs")
+    const Router = require("koa-router")
+    const koaStaticCache = require('koa-static-cache');
+    var cors = require('koa2-cors');
+    const app = new Koa()
+    const router = new Router()
+    app.use(
+        cors({
+            origin: function(ctx) { //设置允许来自指定域名请求
+                return '*';
+            },
+            maxAge: 5,
+            credentials: true,
+            allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+            exposeHeaders: ['WWW-Authenticate', 'Server-Authorization']
+        })
+    );
+
+
+    app.use(koaStaticCache(__dirname + "/dist", {
+        prefix: '/dist',
+        gzip: true
+    }))
+    router.get('/', (ctx) => {
+        console.log('进入到页面');
+        const content = fs.readFileSync('./dist/index.html');
+        ctx.body = content.toString();
+    })
+
+
+    // 挂载路由
+    app.use(router.routes())
+    app.listen(8080); //如果端口占用，这里修改
+    console.log('服务器开启');
+})()
